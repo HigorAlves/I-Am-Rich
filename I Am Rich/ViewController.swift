1be001eb34ff7f4fb14480009cac8572cdc10a7c
Higor Alves <higorhaalves@gmail.com>
2020-07-25 12:37:28 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Higor Alves on 25/07/20.
+//  Copyright © 2020 Higor Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
